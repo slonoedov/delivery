b3d1e993c6ac93260182f4b2a4abb9cd392d6736
slonoedov <slonoedov63@gmail.com>
2020-04-10 14:14:00 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log('jdgjdjgdnk');
+const cartButton = document.querySelector("#cart-button");
+const modal= document.querySelector(".modal");
+const close = document.querySelector(".close");
+cartButton.addEventListener("click", function(event){
+    modal.classList.add("is-open");
+});
+close.addEventListener("click", function(event){
+    modal.classList.remove("is-open");
+});
+
+new WOW().init();
